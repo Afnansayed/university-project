eff3c1e67757c2685233b355795e43b6a10f3eed
Afnansayed <afnansayed1973@gmail.com>
2024-02-16 00:45:22 +0600

    large amount off code

--- script/all.js
+++ script/all.js
@@ -1,1 +1,60 @@
-console.log('Hi how are you')
+//start
+function start(){
+    addClass('game-start');
+    hideClass('play-ground')
+    addClass('score');
+
+    //set stating time life
+    scoreUpdate('current-life',5);
+    //set starting time score
+    scoreUpdate('current-score',0);
+}
+
+document.addEventListener('keyup',function(event){
+    const playerPressedKey = event.key;
+    //for exit game
+    if(playerPressedKey === 'Escape'){
+        gameOver();
+     }
+    //
+    const currentAlphabet = document.getElementById('current-alphabet');
+    const alphabet = currentAlphabet.innerText;
+    const convertLowerCase = alphabet.toLowerCase();
+
+    if(playerPressedKey === convertLowerCase){
+        console.log('you are right path')
+       const currentScore = scoreHoldElement('current-score');
+       //updated score
+       const updatedScore = currentScore + 1;
+       //set score
+       scoreUpdate('current-score',updatedScore);
+       console.log(updatedScore)
+    }else{
+        console.log('you are rong path')
+        const currentLife = scoreHoldElement('current-life')
+        //update life
+        const updateLife = currentLife -1;
+        //set life
+        scoreUpdate('current-life',updateLife);
+
+        if(updateLife === 0){
+            gameOver();
+        }
+    }
+    continueGame();
+})
+
+//continue game
+function continueGame(){
+    const alphabet = randomAlphabet();
+    //set this randomly got alphabet into the display showing alphabet
+    const setAlphabet = document.getElementById('current-alphabet');
+    setAlphabet.innerText = alphabet;
+}
+//Game Over function
+function gameOver(){
+    addClass('play-ground');
+    hideClass('score');
+
+    //set final score
+}
--- script/reuse.js
+++ script/reuse.js
@@ -1,1 +1,31 @@
-console.log('happy velantine day');
+function addClass(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+function hideClass(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+//for a random alpabet
+function randomAlphabet(){
+     // get or create an alphabet array
+    const alphabets = 'qwertyuiopasdfghjklzxcvbnm';
+    const alpabetArray = alphabets.slice('');
+     //get a random index 0-25
+    const ranNumber = Math.random()*25;
+    const index = Math.round(ranNumber);
+    //get alphabet holding by index
+    const alphabet = alpabetArray[index];
+    return alphabet;
+}
+//for score
+function scoreHoldElement(elementId){
+   const element =  document.getElementById(elementId);
+   const currentScoreText = element.innerText;
+   const currentScore = parseInt(currentScoreText);
+   return currentScore;
+}
+function scoreUpdate(elementId,value){
+    const element =  document.getElementById(elementId);
+    element.innerText = value;
+}
